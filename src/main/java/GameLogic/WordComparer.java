cb127b0e9c8257a490b461576794ffff90645bff
Raphael Haemmerli <raphael.haemmerli@uzh.ch>
2020-04-05 12:29:20 +0200

    IntroducedWordComparer and tests for it
    Word comparer gets a list of clues and returnes all clues that are not duplicates (or their stems are duplicates)

---
+++ src/main/java/GameLogic/WordComparer.java
@@ -0,0 +1,33 @@
+package GameLogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordComparer {
+    private ArrayList<String> helperWords;
+
+    public ArrayList<String> compareClues(ArrayList<String> clues){
+        ArrayList<String> okClues = new ArrayList<String>();
+        ArrayList<String> wordStems = new ArrayList<String>();
+        ArrayList<String> duplicates = new ArrayList<String>();
+        for (String word : clues) {
+            String stem = this.simulateApi(word);       //get the word stem from API
+            if (wordStems.contains(stem)){duplicates.add(stem);} //if stem is already in wordStems, it's a duplicate
+            wordStems.add(stem);    //add stem to stemlist
+        }
+        for (int i = 0; i < clues.size(); i++) {
+
+            if (!duplicates.contains(wordStems.get(i))){    //only add words that are not duplicates
+                okClues.add(clues.get(i));
+            }
+        }
+        return okClues;
+    }
+
+    public String simulateApi(String s) {
+        if (s == "test1") {return "test1";}
+        if (s == "test2") {return "test2";}
+        return "princ";
+    }
+
+}
---
+++ src/test/java/GameLogic/WordComparerTest.java
@@ -0,0 +1,91 @@
+package GameLogic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+
+
+class WordComparerTest {
+
+    @Test
+    public void noDuplicateInClues(){
+        WordComparer wordComparer = new WordComparer();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("test1");
+        words.add("test2");
+        words.add("test3");
+        ArrayList<String> actual = wordComparer.compareClues(words);
+        assertEquals(words, actual);
+    }
+
+    @Test
+    public void OneDuplicateInClues(){
+        WordComparer wordComparer = new WordComparer();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("test1");
+        words.add("test1");
+        words.add("test3");
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("test3");
+        ArrayList<String> actual = wordComparer.compareClues(words);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TwoDuplicateInClues(){
+        WordComparer wordComparer = new WordComparer();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("test1");
+        words.add("test1");
+        words.add("test2");
+        words.add("test2");
+        words.add("test3");
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("test3");
+        ArrayList<String> actual = wordComparer.compareClues(words);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TriplicateInClues(){
+        WordComparer wordComparer = new WordComparer();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("test1");
+        words.add("test1");
+        words.add("test1");
+        words.add("test3");
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("test3");
+        ArrayList<String> actual = wordComparer.compareClues(words);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void OnlyDuplicatesInClues(){
+        WordComparer wordComparer = new WordComparer();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("test1");
+        words.add("test1");
+        words.add("test1");
+        ArrayList<String> expected = new ArrayList<String>();
+        ArrayList<String> actual = wordComparer.compareClues(words);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestStemAPI(){
+        WordComparer wordComparer = new WordComparer();
+        String expected = "princ";
+        String actual1 = wordComparer.simulateApi("prince");
+        String actual2 = wordComparer.simulateApi("princess");
+        assertEquals(expected, actual1);
+        assertEquals(expected, actual2);
+    }
+
+
+
+
+}
