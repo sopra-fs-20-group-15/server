f9847c8a3ed036828f3f7bec9bf3cbb0eb7ed499
Raphael Haemmerli <raphael.haemmerli@uzh.ch>
2020-04-09 10:07:42 +0200

    ComepareMysteryWordsandCompoareClues
    And tests

--- src/main/java/GameLogic/WordComparer.java
+++ src/main/java/GameLogic/WordComparer.java
@@ -1,33 +1,127 @@
 package GameLogic;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
 
 public class WordComparer {
     private ArrayList<String> helperWords;
 
-    public ArrayList<String> compareClues(ArrayList<String> clues){
+    /*
+
+     */
+    public ArrayList<String> compareClues(ArrayList<String> clues) {
         ArrayList<String> okClues = new ArrayList<String>();
+        ArrayList<String> tmpClues = new ArrayList<String>();
         ArrayList<String> wordStems = new ArrayList<String>();
         ArrayList<String> duplicates = new ArrayList<String>();
         for (String word : clues) {
-            String stem = this.simulateApi(word);       //get the word stem from API
+            String stem;
+            try {
+                stem = this.getWordStem(word.toLowerCase());
+            } catch(IOException ex) {
+                stem = word.toLowerCase();
+            }//get the word stem from API
             if (wordStems.contains(stem)){duplicates.add(stem);} //if stem is already in wordStems, it's a duplicate
             wordStems.add(stem);    //add stem to stemlist
         }
         for (int i = 0; i < clues.size(); i++) {
 
             if (!duplicates.contains(wordStems.get(i))){    //only add words that are not duplicates
-                okClues.add(clues.get(i));
+                tmpClues.add(clues.get(i));
             }
         }
+        //checks that none of the clues are to close
+        for (String okClue : tmpClues){
+            int count = 0;
+            for (String word : clues){
+                if (this.closeWords(okClue, word)){
+                    count++;
+                }
+            }
+            if (count < 2) {
+                okClues.add(okClue);
+            }
+        }
+
         return okClues;
     }
 
-    public String simulateApi(String s) {
-        if (s == "test1") {return "test1";}
-        if (s == "test2") {return "test2";}
-        return "princ";
+    public boolean compareMysteryWords(String guess, String mysterWord){
+        return closeWords(guess, mysterWord);
+    }
+
+    /*
+    gets two strings and returns boolean if they are closely the same
+     */
+    protected boolean closeWords(String word1, String word2){
+        word1 = word1.toLowerCase();
+        word2 = word2.toLowerCase();
+        if (word1.length() != word2.length()){
+            return false;
+        }
+        int count = 0;
+        if (word1.length() < 2 || word2.length() < 2){
+            return word1 == word2;
+        }
+        if (word1.charAt(0) != word2.charAt(0)) {
+            return false;
+        }
+        for (int i = 1; i < word1.length(); i++) {
+            if (word1.charAt(i) == word2.charAt(i)){
+                count++;
+            }
+        }
+
+        return count >= word1.length()-2;
+    }
+
+    protected String getWordStem(String s) throws IOException {
+        String apiAnswer;
+
+        String urlParameters = "text=" + s;
+        byte[] postData = urlParameters.getBytes( StandardCharsets.UTF_8 );
+        int postDataLength = postData.length;
+        String request = "http://text-processing.com/api/stem/";
+        URL url = new URL( request );
+        HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setInstanceFollowRedirects(false);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("charset", "utf-8");
+        conn.setRequestProperty("Content-Length", Integer.toString(postDataLength ));
+        conn.setUseCaches(false);
+        try(DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+            wr.write(postData);
+            int responseCode = conn.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            apiAnswer = response.toString();
+        }
+        String stem = convertApiAnswer(apiAnswer);
+        return stem;
+    }
+
+    /**
+     * @param s the string StemApiForm
+     * @return  word Stem
+     */
+    private String convertApiAnswer(String s){
+        int middle = s.indexOf(":");
+        String newString = s.substring(middle+3, s.length()-2);
+        return newString;
+
     }
 
 }
--- src/test/java/GameLogic/WordComparerTest.java
+++ src/test/java/GameLogic/WordComparerTest.java
@@ -1,91 +1,164 @@
 package GameLogic;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 import java.util.ArrayList;
-
-
 
 class WordComparerTest {
 
     @Test
-    public void noDuplicateInClues(){
+    public void noDuplicateInClues() {
         WordComparer wordComparer = new WordComparer();
         ArrayList<String> words = new ArrayList<String>();
-        words.add("test1");
-        words.add("test2");
-        words.add("test3");
+        words.add("Tree");
+        words.add("live");
+        words.add("house");
         ArrayList<String> actual = wordComparer.compareClues(words);
         assertEquals(words, actual);
     }
 
     @Test
-    public void OneDuplicateInClues(){
+    public void OneDuplicateInClues() {
         WordComparer wordComparer = new WordComparer();
         ArrayList<String> words = new ArrayList<String>();
-        words.add("test1");
-        words.add("test1");
-        words.add("test3");
+        words.add("live");
+        words.add("living");
+        words.add("Tree");
         ArrayList<String> expected = new ArrayList<String>();
-        expected.add("test3");
+        expected.add("Tree");
         ArrayList<String> actual = wordComparer.compareClues(words);
+
         assertEquals(expected, actual);
     }
 
     @Test
-    public void TwoDuplicateInClues(){
+    public void TwoDuplicateInClues() {
         WordComparer wordComparer = new WordComparer();
         ArrayList<String> words = new ArrayList<String>();
-        words.add("test1");
-        words.add("test1");
-        words.add("test2");
-        words.add("test2");
-        words.add("test3");
+        words.add("live");
+        words.add("living");
+        words.add("Horse");
+        words.add("horses");
+        words.add("Tree");
         ArrayList<String> expected = new ArrayList<String>();
-        expected.add("test3");
+        expected.add("Tree");
         ArrayList<String> actual = wordComparer.compareClues(words);
+
         assertEquals(expected, actual);
     }
 
     @Test
-    public void TriplicateInClues(){
+    public void TriplicateInClues() {
         WordComparer wordComparer = new WordComparer();
         ArrayList<String> words = new ArrayList<String>();
-        words.add("test1");
-        words.add("test1");
-        words.add("test1");
-        words.add("test3");
+        words.add("live");
+        words.add("life");
+        words.add("living");
+        words.add("house");
         ArrayList<String> expected = new ArrayList<String>();
-        expected.add("test3");
+        expected.add("house");
         ArrayList<String> actual = wordComparer.compareClues(words);
+
         assertEquals(expected, actual);
     }
 
     @Test
-    public void OnlyDuplicatesInClues(){
+    public void OnlyDuplicatesInClues() {
         WordComparer wordComparer = new WordComparer();
         ArrayList<String> words = new ArrayList<String>();
-        words.add("test1");
-        words.add("test1");
-        words.add("test1");
+        words.add("lives");
+        words.add("lifes");
+        words.add("life");
         ArrayList<String> expected = new ArrayList<String>();
         ArrayList<String> actual = wordComparer.compareClues(words);
+
         assertEquals(expected, actual);
     }
 
     @Test
-    public void TestStemAPI(){
+    public void TestStemAPI() {
         WordComparer wordComparer = new WordComparer();
-        String expected = "princ";
-        String actual1 = wordComparer.simulateApi("prince");
-        String actual2 = wordComparer.simulateApi("princess");
+        String expected = "live";
+        String actual1;
+        String actual2;
+        try {
+            actual1 = wordComparer.getWordStem("live");
+            actual2 = wordComparer.getWordStem("living");
+        } catch(IOException ex) {
+            actual1 = "";
+            actual2 = "";
+        }
         assertEquals(expected, actual1);
         assertEquals(expected, actual2);
     }
 
+    @Test
+    public void TestCloseWordsNoMistakes() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "Azkaban";
+        String word2 = "Azkaban";
 
+        assertTrue(wordComparer.closeWords(word1, word2));
+    }
+
+    @Test
+    public void TestCloseWordsDifferentCapitalization() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "ASKaBan";
+        String word2 = "Askaban";
+
+        assertTrue(wordComparer.closeWords(word1, word2));
+    }
+
+    @Test
+    public void TestCloseWordsNotCloseEnough() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "house";
+        String word2 = "Mouse";
+
+        assertFalse(wordComparer.closeWords(word1, word2));
+    }
+
+    @Test
+    public void TestCloseWordsOneMistake() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "Askaban";
+        String word2 = "Azkaban";
+
+        assertTrue(wordComparer.closeWords(word1, word2));
+    }
+
+    @Test
+    public void TestCloseWordsTwoMistakes() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "Askaban";
+        String word2 = "Azcaban";
+
+        assertFalse(wordComparer.closeWords(word1, word2));
+    }
+
+    @Test
+    public void TestCompareMysteryWordsCorrect() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "Askaban";
+        String word2 = "Azkaban";
+
+        assertTrue(wordComparer.compareMysteryWords(word1, word2));
+    }
+
+    @Test
+    public void TestCompareMysteryWordsWrong() {
+        WordComparer wordComparer = new WordComparer();
+        String word1 = "Dog";
+        String word2 = "Azkaban";
+
+        assertFalse(wordComparer.compareMysteryWords(word1, word2));
+    }
 
 
 }
