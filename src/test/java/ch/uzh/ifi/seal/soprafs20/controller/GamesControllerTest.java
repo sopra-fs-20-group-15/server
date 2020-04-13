067a3782f84b672c5e216c06143f939814ed0437
CortexVacua <45538598+CortexVacua@users.noreply.github.com>
2020-04-13 02:43:44 +0200

    Solved Merge Conflicts

--- src/test/java/ch/uzh/ifi/seal/soprafs20/controller/GamesControllerTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/controller/GamesControllerTest.java
@@ -4,6 +4,7 @@ import ch.uzh.ifi.seal.soprafs20.Entities.GameEntity;
 import ch.uzh.ifi.seal.soprafs20.Entities.GameSetUpEntity;
 import ch.uzh.ifi.seal.soprafs20.Entities.PlayerEntity;
 import ch.uzh.ifi.seal.soprafs20.rest.dto.*;
+import ch.uzh.ifi.seal.soprafs20.service.CardService;
 import ch.uzh.ifi.seal.soprafs20.service.GameService;
 import ch.uzh.ifi.seal.soprafs20.exceptions.*;
 import ch.uzh.ifi.seal.soprafs20.service.PlayerService;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
@@ -49,6 +51,9 @@ public class GamesControllerTest {
 
     @MockBean
     private PlayerService playerService;
+
+    @MockBean
+    private CardService cardService;
 
     /**
      * Tests a post-Request to /games/
