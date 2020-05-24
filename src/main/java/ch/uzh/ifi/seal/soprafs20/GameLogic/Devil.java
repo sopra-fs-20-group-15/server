44ed0838384d0c31fd3b285be32ab8fd8405ab99
Raphael Haemmerli <raphael.haemmerli@uzh.ch>
2020-05-24 21:26:13 +0200

    M4

--- src/main/java/ch/uzh/ifi/seal/soprafs20/GameLogic/Devil.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/GameLogic/Devil.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Embeddable
 public class Devil implements Bot {
@@ -35,13 +36,12 @@ public class Devil implements Bot {
         if (n < 0 || n> 4) {
             return this.randomWord();
         }
-        Random rand = new Random();
         String[] words = line.split(" ");
         int r = words.length/5;
         if (words.length % 5 > n) {
             r++;
         }
-        int rn = rand.nextInt(r) * 5+ n;
+        int rn = ThreadLocalRandom.current().nextInt(r) * 5+ n;
         return words[rn];
     }
 
@@ -107,64 +107,4 @@ public class Devil implements Bot {
         return botToken;
     }
 
-    /*
-    When updating the cardsEnDevil File for new mystery>words, you will need those,
-    but since they change the file and thus could break it, they are commented out, so nobody uses them.
-
-    public List<String> giveMoleList(String mysteryWord) {
-        ApiRequester apiRequester = new ApiRequester();
-        Angel angel = new Angel();
-        WordComparer wordComparer = new WordComparer();
-        List<String> relWords = new ArrayList<>();
-        try {
-            relWords = apiRequester.getFiveWordsFromDatamuseApi(mysteryWord, "ml");
-        } catch (IOException ex) {
-        }
-        if (relWords.size() < 3) {
-            relWords.add("Error");
-            relWords.add(this.randomWord());
-            relWords.add(this.randomWord());
-        }
-        List<String> moleClues = new ArrayList<>();
-        try {
-            moleClues = apiRequester.getFiveWordsFromDatamuseApi(relWords.get(2), "ml");
-            moleClues.addAll(apiRequester.getFiveWordsFromDatamuseApi(relWords.get(1), "ml"));
-            moleClues.addAll(apiRequester.getFiveWordsFromDatamuseApi(relWords.get(1), "rel_trg"));
-        } catch (IOException ex) { }
-
-        List<String> goodClues = angel.giveClueList(mysteryWord);
-        this.removeToHelpfulClues(goodClues, moleClues);
-
-        wordComparer.notSuitableBotClue(moleClues, mysteryWord);
-        Map<String, Integer> check = wordComparer.compareClues((ArrayList<String>) moleClues, mysteryWord);
-        for (Map.Entry<String, Integer> entry : check.entrySet()) {
-            if (entry.getValue() > 0) {
-                moleClues.remove(entry.getKey());
-            }
-        }
-
-        return moleClues;
-    }
-
-    //used to write clues into the file
-    try {
-            FileWriter fw = new FileWriter("cardsEnDevil");
-            for (int i = 0; i < listOfLines.size(); i++) {
-                String mWord = listOfLines.get(i);
-                List<String> clues = devil.giveMoleList(mWord);
-                fw.write(mWord + ": ");
-                for (int j = 0; j < clues.size(); j++){
-                    fw.write(clues.get(j) + " ");
-                }
-                if (clues.isEmpty()) {
-
-                    System.out.println("Empty: " + mWord + " number:" + i);
-                }
-                fw.write("\n");
-            }
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("ErrorWriter");
-        }
-     */
 }
